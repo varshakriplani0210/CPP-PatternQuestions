f26240c96fbc8eec02c9d8542f50cc611a057814
varsha kriplani <72504188+varshakriplani0210@users.noreply.github.com>
2021-07-05 14:13:25 +0530

    Hill Pattern
              * 
            * * * 
          * * * * * 
        * * * * * * * 
      * * * * * * * * * 
    
    See Its visualization on :
    http://pythontutor.com/cpp.html#code=%23include%20%3Ciostream%3E%0A%0Ausing%20namespace%20std%3B%0A%0Aint%20main%28%29%0A%7B%0A%20%20%20%20int%20i,j,n%3D5%3B%0A%20%20%20%20for%28i%3D1%3Bi%3C%3Dn%3Bi%2B%2B%29%0A%20%20%20%20%7B%0A%20%20%20%20%20for%28j%3Di%3Bj%3C%3Dn%3Bj%2B%2B%29%0A%20%20%20%20%20%7B%0A%20%20%20%20%20%20%20%20%20cout%3C%3C%22%20%20%22%3B%0A%20%20%20%20%20%7D%0A%20%20%20%20%20for%28j%3D1%3Bj%3Ci%3Bj%2B%2B%29%0A%20%20%20%20%20%7B%0A%20%20%20%20%20%20%20%20%20cout%3C%3C%22*%20%22%3B%0A%20%20%20%20%20%7D%0A%20%20%20%20%20%20%20for%28j%3D1%3Bj%3C%3Di%3Bj%2B%2B%29%0A%20%20%20%20%20%7B%0A%20%20%20%20%20%20%20%20%20cout%3C%3C%22*%20%22%3B%0A%20%20%20%20%20%7D%20%0A%20%20%20%20%20%20%20%20%0A%20%20%20%20%20%20%20%20cout%3C%3Cendl%3B%0A%20%20%20%20%7D%0A%7D%0A&curInstr=79&mode=display&origin=opt-frontend.js&py=cpp_g%2B%2B9.3.0&rawInputLstJSON=%5B%5D

---
+++ HillPattern.cpp
@@ -0,0 +1,35 @@
+/*
+          * 
+        * * * 
+      * * * * * 
+    * * * * * * * 
+  * * * * * * * * * 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,j,n;
+    cout<<"enter n";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+     for(j=i;j<=n;j++)
+     {
+         cout<<"  ";
+     }
+     for(j=1;j<i;j++)
+     {
+         cout<<"* ";
+     }
+       for(j=1;j<=i;j++)
+     {
+         cout<<"* ";
+     } 
+        
+        cout<<endl;
+    }
+}
