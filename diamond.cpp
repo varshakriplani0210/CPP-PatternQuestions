6f2f11bc9c3175ea6b599610c255a82d56065ba3
varsha kriplani <72504188+varshakriplani0210@users.noreply.github.com>
2021-07-05 14:58:57 +0530

    Diamond.cpp
         *
        ***
       *****
      *******
     *********
     *********
      *******
       *****
        ***
         *

---
+++ diamond.cpp
@@ -0,0 +1,62 @@
+/*
+
+     *
+    ***
+   *****
+  *******
+ *********
+ *********
+  *******
+   *****
+    ***
+     *
+     
+*/     
+     
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+  int i,j,n;
+  cout<<"enter n ";
+  cin>>n;
+  for(i=1;i<=n;i++)
+    {
+     for(j=i;j<=n;j++)
+     {
+         cout<<" ";
+     }
+     for(j=1;j<i;j++)
+     {
+         cout<<"*";
+     }
+       for(j=1;j<=i;j++)
+     {
+         cout<<"*";
+     } 
+        
+        cout<<endl;
+    }
+  for(i=1;i<=n;i++)
+  {
+      for(j=1;j<=i;j++)
+      {
+          cout<<" ";
+      }
+      
+      for(j=i;j<n;j++){
+         cout<<"*";
+      }
+      for(j=i;j<=n;j++){
+          cout<<"*";
+      }
+      
+      
+      cout<<endl;
+  }
+  return 0;
+}
+
