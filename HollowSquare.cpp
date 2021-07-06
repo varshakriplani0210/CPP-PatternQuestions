132f385963f9bbd83ea13a9a5f54287333a6be86
varsha kriplani <72504188+varshakriplani0210@users.noreply.github.com>
2021-07-06 19:04:12 +0530

    Hollow Square Pattern in cpp
    * * * * * 
    *        * 
    *        * 
    *        * 
    * * * * *

---
+++ HollowSquare.cpp
@@ -0,0 +1,36 @@
+/*
+
+* * * * * 
+*       * 
+*       * 
+*       * 
+* * * * *
+
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,j,n;
+    cout<<"enter n ";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+             if(i==1 || i==n || j==1 || j==n)
+        {
+            cout<<"* ";
+        }
+        else{
+            cout<<"  ";
+        }
+        }
+        
+       
+        
+        cout<<endl;
+    }
+}
